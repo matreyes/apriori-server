b77e0a0b0d0146f90830138e6062eba51bce4c68
Bernard Banta <banta.bernard@gmail.com>
2013-04-23 18:20:22 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AprioriServer::Application.config.secret_token = '5977332b3e895beb6352689bb3702ef1ce6fbd929a7c7f32ee6fad09759f503bcf486729e97bca96492f7fb9124d9d31ed61a77ff50b05b6be3e4f55e00cb164'
